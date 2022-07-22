576aa3f63fca017a52517608b71f425558dc6300
sridharp45 <psridhar@iitg.ac.in>
2022-07-23 02:32:17 +0530

    fixing audio

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 score = 0;
 cross = true;
 
-// audio = new Audio('music.mp3');
-// audiogo = new Audio('gameover.mp3');
-// setTimeout(() => {
-//     audio.play()
-// }, 1000);
+audio = new Audio('music.mp3');
+audiogo = new Audio('gameover.mp3');
+setTimeout(() => {
+    audio.play()
+}, 1000);
 document.onkeydown = function (e) {
     console.log("Key code is: ", e.keyCode)
     if (e.keyCode == 38) {
